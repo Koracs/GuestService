4b5e1ee0a70c39062adfa35f538fa5e1a34449cc
Koracs <115572388+Koracs@users.noreply.github.com>
2023-11-04 22:49:36 +0100

    add edit function

---
+++ frontend/app/bookings/[id]/edit/page.jsx
@@ -0,0 +1,36 @@
+import prisma from '../../../db'
+import Link from "next/link";
+
+async function editBooking() {
+    "use server"
+    //todo: implement this function
+    //todo multiple server actions from one form? (edit and delete)
+}
+
+export default async function NewPage({params}) {
+    console.log("JSON.stringify(params)")
+    console.log(JSON.stringify(params))
+    const booking = await prisma.booking.findUnique({where: {id: parseInt(params?.id)}});
+
+    return (
+        <>
+            <h1>Edit Booking {params?.id}</h1>
+            <form action={editBooking}>
+                <label htmlFor="room">Room</label>
+                <input type="number" id="room" name="room" required={true} value={booking?.room}/>
+                <br/>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name="name" required={true} value={booking?.name}/>
+                <br/>
+                <label htmlFor="fromDate">From Date</label>
+                <input type="date" id="fromDate" name="fromDate" required={true} value={booking?.fromDate?.toLocaleDateString()}/>
+                <br/>
+                <label htmlFor="toDate">To Date</label>
+                <input type="date" id="toDate" name="toDate" required={true} value={booking?.toDate?.toLocaleDateString()}/>
+                <br/>
+                <br/>
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    )
+}
--- frontend/app/bookings/[id]/page.jsx
+++ frontend/app/bookings/[id]/page.jsx
@@ -1,4 +1,5 @@
 import prisma from '../../db'
+import Link from "next/link";
 
 export default async function NewPage({params}) {
     console.log("JSON.stringify(params)")
@@ -8,10 +9,19 @@ export default async function NewPage({params}) {
     return (
         <>
             <h1>Booking {params?.id}</h1>
-            <p>Room: {booking?.room}</p>
-            <p>Name: {booking?.name}</p>
-            <p>From Date: {booking?.fromDate?.toLocaleDateString()}</p>
-            <p>To Date: {booking?.toDate?.toLocaleDateString()}</p>
+            <p>Room Number: {booking?.room}</p>
+            <p>Guest Name: {booking?.name}</p>
+            <br/>
+            <p>Booking From Date: {booking?.fromDate?.toLocaleDateString()}</p>
+            <p>Booking To Date: {booking?.toDate?.toLocaleDateString()}</p>
+            <br/>
+            <p>Created At: {booking?.createdAt?.toLocaleDateString()}</p>
+            <p>Updated At: {booking?.updatedAt?.toLocaleDateString()}</p>
+
+            //create link to edit booking
+            <br/>
+            <br/>
+            <Link href={`/bookings/${params?.id}/edit`}>Edit</Link>
         </>
     )
 }
